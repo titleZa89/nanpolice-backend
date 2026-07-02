05438af2624abff6766616727220e2df33b8608a
Nutchanon547 <32757969+titleZa89@users.noreply.github.com>
2026-07-01 21:05:12 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -32,7 +32,9 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: { rejectUnauthorized: false }
+  ssl: {
+    rejectUnauthorized: false // จำเป็นต้องมีสำหรับ Aiven
+  }
 });
 
 db.connect((err) => {
